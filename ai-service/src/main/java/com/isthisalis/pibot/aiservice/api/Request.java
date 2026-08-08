cfc3abf85b5f111535bd62e039aa41167fb8956d
IsThisALis <gamemodebronze2@gmail.com>
2026-08-08 10:00:33 +0500

    docs(Request): add javadoc

--- ai-service/src/main/java/com/isthisalis/pibot/aiservice/api/Request.java
+++ ai-service/src/main/java/com/isthisalis/pibot/aiservice/api/Request.java
@@ -3,6 +3,17 @@ package com.isthisalis.pibot.aiservice.api;
 import lombok.Value;
 
 @Value
+/**
+ * Data-class representing request to this service.
+ */
 public class Request {
+    /** 
+     * Authentification header value.
+     **/
+    String xApiKey;
+
+    /** 
+     * Request content.
+     **/
     String message;
 }
